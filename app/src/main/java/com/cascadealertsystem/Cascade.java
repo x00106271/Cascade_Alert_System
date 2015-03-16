320b1b0b1c3dca13f2851942e8e111c20de72ced
x00106271 <x00106271@outlook.com>
2015-03-16 16:42:50 +0000

    android version 2

---
+++ app/src/main/java/com/cascadealertsystem/Cascade.java
@@ -0,0 +1,20 @@
+package com.cascadealertsystem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class Cascade extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+        // bring user to login screen
+        Intent loginScreen = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(loginScreen);
+        finish();
+    }
+
+}
