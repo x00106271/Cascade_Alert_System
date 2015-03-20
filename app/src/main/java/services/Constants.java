3e67e6c838169eebf3ac8f1d666635453cd3996f
x00106271 <x00106271@outlook.com>
2015-03-20 16:32:41 +0000

    mobile services update

---
+++ app/src/main/java/services/Constants.java
@@ -0,0 +1,8 @@
+package services;
+
+
+public final class Constants {
+
+    public static String MOBILE_SERVICE_URL = "https://cascade.azure-mobile.net/";
+    public static String MOBILE_SERVICE_APPLICATION_KEY = "TVkGQDWdaeNNbNKirTHAknOUmHqWgu76";
+}
---
+++ app/src/main/java/services/MobileService.java
@@ -0,0 +1,7 @@
+package services;
+
+/**
+ * Created by Caroline on 20/03/2015.
+ */
+public class MobileService {
+}
